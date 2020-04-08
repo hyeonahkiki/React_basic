26da8d1d3a758e904aac99369233c436196defef
hyeonahkiki <kkongc7@gmail.com>
2020-04-08 15:08:29 +0900

    use BrowseRouter, Switch, Route from react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import React, { useState } from "react";
+import React from "react";
 import Counter from "./components/counter";
 import Cat from "./components/cat";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Nav from "./components/nav";
 
+//Switch : java switch문같은 느낌
+//Route path="경로입력" component="보여줄 components"
+// /일때 counter만들려면 exact를 써준다.
+// BrowserRouter :
 function App() {
   return (
-    <>
-      <Counter />
-      <Cat />
-    </>
+    <BrowserRouter>
+      <Nav />
+      <Switch>
+        <Route path="/" component={Counter} exact />
+        <Route path="/cat" component={Cat} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
